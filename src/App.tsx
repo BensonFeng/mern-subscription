0daff8fbb4bd721cdafbc0ac56157bd243b06ffb
BensonFeng <bensonfeng@outlook.com>
2022-03-29 12:00:31 -0400

    bootstrap & styled components: nav, hero

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div>
       <Nav />
+      <Hero />
     </div>
   );
 }
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Navbar, NavItem, NavLink } from "react-bootstrap";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <Navbar>
+      <NavItem>
+        <NavLink>NavBar</NavLink>
+      </NavItem>
+    </Navbar>
+  );
 };
 
 export default Nav;
