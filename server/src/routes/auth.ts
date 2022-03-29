6f415348423b450bb0a3ebc021a1aebb44dce7c2
BensonFeng <bensonfeng@outlook.com>
2022-03-29 16:42:02 -0400

    server: email password validator express-validator

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,14 +1,34 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  res.json({
-    email,
-    password,
-  });
-});
+router.post(
+  "/signup",
+  body("email").isEmail().withMessage("The email is invalid"),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("The password is too short"),
+  async (req: express.Request, res: express.Response) => {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+      const error = validationErrors.array().map((error) => {
+        return {
+          msg: error.msg,
+        };
+      });
+      return res.json({ error });
+    }
+
+    const { email, password } = req.body;
+
+    console.log(email, password);
+
+    res.json({
+      email,
+      password,
+    });
+  }
+);
 
 export default router;
