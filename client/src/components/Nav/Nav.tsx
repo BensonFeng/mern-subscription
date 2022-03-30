8dc5ec4ab8933d13f80f23e0e6e06f608534d163
BensonFeng <bensonfeng@outlook.com>
2022-03-30 17:16:10 -0400

    client: display logout btn when user signin

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -9,6 +9,13 @@ const Nav = () => {
           Home
         </Link>
       </NavItem>
+      {localStorage.getItem("token") && (
+        <NavItem>
+          <Link to="/" className="nav-link">
+            Logout
+          </Link>
+        </NavItem>
+      )}
     </Navbar>
   );
 };
