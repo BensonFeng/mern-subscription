eb70742707894b6e526461eaaf8ce0d359cc0dd4
BensonFeng <bensonfeng@outlook.com>
2022-03-31 14:41:57 -0400

    client: validate token from context api to display logout btn

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,7 +1,15 @@
 import { Navbar, NavItem, NavLink } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../context";
+import styled from "styled-components";
+
+const LeftNavContainer = styled.div`
+  margin-left: auto;
+`;
 
 const Nav = () => {
+  const [state, setState] = useContext(UserContext);
   return (
     <Navbar>
       <NavItem>
@@ -9,6 +17,13 @@ const Nav = () => {
           Home
         </Link>
       </NavItem>
+      {state.data && (
+        <LeftNavContainer>
+          <NavItem>
+            <NavLink>Logout</NavLink>
+          </NavItem>
+        </LeftNavContainer>
+      )}
     </Navbar>
   );
 };
