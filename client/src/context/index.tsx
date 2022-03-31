67f997f4b905bf25c6982b3344d29a52fd724272
BensonFeng <bensonfeng@outlook.com>
2022-03-31 14:03:14 -0400

    client: create context api fetch user auth credential

--- client/src/context/index.tsx
+++ client/src/context/index.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { createContext, useEffect, useState } from "react";
 
 interface User {
@@ -19,4 +20,51 @@ const UserProvider = ({ children }: any) => {
     loading: true,
     error: null,
   });
+
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    axios.defaults.headers.common["authorization"] = `Bearer ${token}`;
+  }
+
+  const fetchUser = async () => {
+    const { data: response } = await axios.get("http://localhost:8080/auth/me");
+
+    if (response.data && response.data.user) {
+      setUser({
+        data: {
+          id: response.data.user.id,
+          email: response.data.user.email,
+        },
+        loading: false,
+        error: null,
+      });
+    } else if (response.data && response.data.errors.length) {
+      setUser({
+        data: null,
+        loading: false,
+        error: response.errors[0].msg,
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      fetchUser();
+    } else {
+      setUser({
+        data: null,
+        loading: false,
+        error: null,
+      });
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider value={[user, setUser]}>
+      {children}
+    </UserContext.Provider>
+  );
 };
+
+export { UserContext, UserProvider };
