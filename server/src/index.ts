9774293134bf87f8e4aa8927d47ddd9ddaa4dce3
BensonFeng <bensonfeng@outlook.com>
2022-04-23 18:07:42 -0400

    final

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth";
-import subRoutes from "./routes/subs";
+import subsRoutes from "./routes/subs";
 import articlesRoutes from "./routes/articles";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -11,14 +11,14 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO_URI as string)
   .then(() => {
-    console.log("connected to mongodb");
+    console.log("Connected to mongodb");
 
     const app = express();
 
     app.use(express.json());
     app.use(cors());
     app.use("/auth", authRoutes);
-    app.use("/subs", subRoutes);
+    app.use("/subs", subsRoutes);
     app.use("/articles", articlesRoutes);
 
     app.listen(8080, () => {
