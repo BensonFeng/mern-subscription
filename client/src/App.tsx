237becb27e35e9dfcb56ecf830d45f1933258004
BensonFeng <bensonfeng@outlook.com>
2022-04-23 18:01:13 -0400

    protected route fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav/Nav";
-import LandingPage from "./pages/LandingPage";
+import Navbar from "./components/Nav/Nav";
 import Articles from "./pages/Articles";
-import ProtectedRoute from "./routes/ProtectedRoute";
 import ArticlesPlan from "./pages/ArticlesPlan";
+import LandingPage from "./pages/LandingPage";
+import { ProtectedRoute } from "./routes/ProtectedRoute";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/articles" element={<ProtectedRoute />}>
-            <Route path="/articles" element={<Articles />} />
-          </Route>
-          <Route path="/article-plans" element={<ProtectedRoute />}>
-            <Route path="/article-plans" element={<ArticlesPlan />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/articles" element={<ProtectedRoute />}>
+          <Route path="/articles" element={<Articles />} />
+        </Route>
+        <Route path="/article-plans" element={<ProtectedRoute />}>
+          <Route path="/article-plans" element={<ArticlesPlan />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
--- client/src/routes/ProtectedRoute.tsx
+++ client/src/routes/ProtectedRoute.tsx
@@ -2,12 +2,10 @@ import { useContext } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import { UserContext } from "../context";
 
-const ProtectedRoute = () => {
-  const [state, setState] = useContext(UserContext);
+export const ProtectedRoute = () => {
+  const [state] = useContext(UserContext);
 
   if (state.loading) return <div>Spinner...</div>;
 
   return state.data ? <Outlet /> : <Navigate to="/" />;
 };
-
-export default ProtectedRoute;
